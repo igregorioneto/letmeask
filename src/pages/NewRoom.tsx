7660d82f48aae10ec04d51daddf25afb7f6363fb
Gregório <igregoriodoneto@outlook.com>
2022-04-24 22:01:06 -0300

    Entrando numa sala já existente e criando nova página de Room

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,13 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { FormEvent, useState } from 'react'
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
+import { useAuth } from './../hooks/useAuth';
 
 import { Button } from '../components/Button';
+import { database } from '../services/firebase';
 
 import '../styles/auth.scss';
 
 export function NewRoom() {
+    const { user } = useAuth();
+    const history = useHistory();
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (newRoom.trim() === '') {
+            return;
+        }
+
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id,
+        });
+
+        history.push(`/rooms/${firebaseRoom.key}`);
+    }
 
     return (
         <div id="page-auth">
@@ -21,13 +44,15 @@ export function NewRoom() {
                 <img src={logoImg} alt="Letmeask" />
                 <h2>Criar uma nova sala</h2>
                 <form>
-                <input 
-                    type="text"
-                    placeholder="Nome da sala"
-                />
-                <Button type="submit">
-                    Criar sala
-                </Button>
+                    <input 
+                        type="text"
+                        placeholder="Nome da sala"
+                        onChange={event => setNewRoom(event.target.value)}
+                        value={newRoom}
+                    />
+                    <Button type="submit">
+                        Criar sala
+                    </Button>
                 </form>
                 <p>
                 Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
